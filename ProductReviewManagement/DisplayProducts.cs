6e5634ec94e4e9f9d86ba2b9975c24985c95c98b
srk-333 <sauravmk333@gmail.com>
2022-02-18 20:21:51 +0530

    [Saurav]Add:Program to get product name from DataTable

---
+++ ProductReviewManagement/DisplayProducts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    public class DisplayProducts
+    {
+        /// <summary>
+        /// Creates the data table.
+        /// </summary>
+        public static void CreateDataTable()
+        {
+            //create obj of DataTable Class.
+            DataTable table = new DataTable();
+            //Add Columns in Table
+            table.Columns.Add("Id");
+            table.Columns.Add("ProductName");
+            //Add Rows in Table
+            table.Rows.Add(1, "Laptop");
+            table.Rows.Add(2, "Desktop");
+            table.Rows.Add(3, "Tab");
+            GetDataFromDataTable(table);
+        }
+        /// <summary>
+        /// Gets the data from data table.
+        /// </summary>
+        /// <param name="table">The table.</param>
+        public static void GetDataFromDataTable(DataTable table)
+        {
+            //Query syntax of LINQ
+            var ProductNames = (from product in table.AsEnumerable() select product.Field<string>("ProductName"));
+            //iterate loop to get ProductName.
+            foreach (string p in ProductNames)
+            {
+                Console.WriteLine("Product Name:"+p);
+            }
+        }
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+            DisplayProducts.CreateDataTable();
+            Console.ReadKey();
+        }
+    }
+}
